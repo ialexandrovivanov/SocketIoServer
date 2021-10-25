deea3f1c5c7fab3ec82f918f30c920d2eb989404
Ivan Alexandrov <ialexandrovivanov@gmail.com>
2021-10-25 11:51:43 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SocketIoServer
+
+This server works with clientServer as part of microservice architecture.
+This server contains two socket io channels for communications with clients and other microservices
+After authentication this is the main server which provides real time comunication with client
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const database = { "smruck@gmail.com": "cheetah" };
+const database = { "smruck@gmail.com": "qqqqqq" };
 
 const CryptoJS = require("crypto-js");
 const ipsKeys = {};
-const optionsClients = { cors: { origin: "http://localhost", methods: ["GET", "POST"] } } //client connection options (cert pem required in production)
+const optionsClients = { cors: { origin: "http://localhost", methods: ["GET", "POST"] } } //client connection options (cert pem for production)
 const optionsServers = { cors: { origin: "http://localhost", methods: ["GET", "POST"] } } //server options allow server ip only
 
 const express = require("express");
@@ -11,8 +11,8 @@ const app = express();
 const serverClients = app.listen(3456, "localhost");
 const serverServers = app.listen(3457, "localhost");
 
-var socketIoClients = require("socket.io")(optionsClients).listen(serverClients);
-var socketIoServers = require("socket.io")(optionsServers).listen(serverServers);
+const socketIoClients = require("socket.io")(optionsClients).listen(serverClients);
+const socketIoServers = require("socket.io")(optionsServers).listen(serverServers);
 
 socketIoClients.on("connect", async function(client) {
 
@@ -30,15 +30,17 @@ socketIoClients.on("connect", async function(client) {
             socketIoClients.to(data.room).emit("cc0462fcafd24bc9b6e527607ad9a3b2", data);
         });
     }
-    else return;
+    else 
+        return;
 });
 
-socketIoServers.on("connect", async function(client) {
+socketIoServers.on("connect", async function(clientServer) {
 
-    // ip filter to pass only server (in development client and server ips are ::1)
-    if (!(client.handshake.address === "::1" || client.handshake.address === "127.0.0.1")) return; 
+    // additional filter to pass only client server ip
+    if (!(clientServer.handshake.address === "::1" || clientServer.handshake.address === "127.0.0.1")) 
+        return; 
 
-    await receiveClientInfo(client.handshake.query);
+    await receiveClientInfo(clientServer.handshake.query);
 });
 
 console.clear();
@@ -46,22 +48,26 @@ console.log("socket.io servers are listening...")
 
 async function receiveClientInfo(query) {
 
-    const keys = require("./privatekeys"); // predefined encryption key for trafic between both servers(can be changed periodically) 
-    const clip = CryptoJS.AES.decrypt(query.clip, keys.cryptoServerKey).toString(CryptoJS.enc.Utf8);
-    const clientIp = clip === "::1" ? "127.0.0.1" : clip;
-    const clientKey = CryptoJS.AES.decrypt(query.temp, keys.cryptoServerKey).toString(CryptoJS.enc.Utf8);
+    const keys = require("./privatekeys"); // predefined encryption key for trafic between servers(can be changed periodically) 
+    const clip = CryptoJS.AES.decrypt(query.clip, keys.cryptoServerKey).toString(CryptoJS.enc.Utf8); // client ip
+    const clientIp = clip === "::1" ? "127.0.0.1" : clip;   // in db records must be in format "127.0.0.1"
+    const clientKey = CryptoJS.AES.decrypt(query.temp, keys.cryptoServerKey).toString(CryptoJS.enc.Utf8); // key for client decryption
 
-    ipsKeys[clientIp] = { clientKey: clientKey, time: Date.now() };
+    ipsKeys[clientIp] = { clientKey: clientKey, time: Date.now() };  // record for each client ip 
 }
 
 async function authClient(query, ip) {
 
-    const startIdx = ipsKeys[ip].clientKey.match(/\d/);
-    const key = ipsKeys[ip].clientKey.substring(startIdx, 11);
-    const email = CryptoJS.AES.decrypt(query.sc7be6bdf90f453b99f1db8f6526518c, key).toString(CryptoJS.enc.Utf8);;
-    const password = CryptoJS.AES.decrypt(query.d2c64414315b4598a5e5fe5832761101, key).toString(CryptoJS.enc.Utf8);
+    try {
+        const startIdx = ipsKeys[ip].clientKey.match(/\d/);
+        const key = ipsKeys[ip].clientKey.substring(startIdx, 11);
+        const email = CryptoJS.AES.decrypt(query.sc7be6bdf90f453b99f1db8f6526518c, key).toString(CryptoJS.enc.Utf8);;
+        const password = CryptoJS.AES.decrypt(query.d2c64414315b4598a5e5fe5832761101, key).toString(CryptoJS.enc.Utf8);
 
-    if (database[email] === password) return true;
+        if (database[email] === password) return true;
 
-    return false;
+        return false;
+
+    } catch (err) { console.log(err); return false; }
+    
 }
